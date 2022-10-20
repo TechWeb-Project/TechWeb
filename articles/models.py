ca453ad89e298179df4aaf90ca18595bb64c08a5
zephyr72836 <zephyr72836@gmail.com>
2022-10-20 17:24:55 +0900

    models 작업(진행중)

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Tech(models.Model):
+    name = models.CharField(max_lenght=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Comapny(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    tech = models.ManyToManyField(Tech, on_delete=models.CASCADE)
